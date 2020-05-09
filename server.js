f23a989befa637282eb5b04e0fcf2900511c7006
Amy Fabella <amyfabella@hotmail.com>
2020-05-09 07:31:33 -0700

    fixed mongoose connection on server.js

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.coonect(MONGODB_URI);
+mongoose.connect(MONGODB_URI);
 
 mongoose.connect("mongodb://localhost/budget", {
   useNewUrlParser: true,
